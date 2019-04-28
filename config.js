1601abdc50b62dfda9cac13d9f88cb5bcc83d5bc
Jelle Hak <jellehak@gmail.com>
2019-04-28 23:05:48 +0200

    Fixes

--- config.js
+++ config.js
@@ -17,5 +17,47 @@ module.exports = {
   },
   'humanReadableOutput': true,
   'collectionOutputType': 'json',
-  'urlPrefix': ''
+  'urlPrefix': '',
+  'schema': {
+    'test': {
+      'users': {
+        'definitions': {},
+        '$schema': 'http://json-schema.org/draft-07/schema#',
+        '$id': 'http://example.com/root.json',
+        'type': 'object',
+        'title': 'The Root Schema',
+        'required': [
+          'price',
+          'tags'
+        ],
+        'properties': {
+          'price': {
+            '$id': '#/properties/price',
+            'type': 'number',
+            'title': 'The Price Schema',
+            'default': 0.0,
+            'examples': [
+              12.5
+            ]
+          },
+          'tags': {
+            '$id': '#/properties/tags',
+            'type': 'array',
+            'title': 'The Tags Schema',
+            'items': {
+              '$id': '#/properties/tags/items',
+              'type': 'string',
+              'title': 'The Items Schema',
+              'default': '',
+              'examples': [
+                'home',
+                'green'
+              ],
+              'pattern': '^(.*)$'
+            }
+          }
+        }
+      }
+    }
+  }
 }
